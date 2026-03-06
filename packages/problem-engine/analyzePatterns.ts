4554a277d11a112f58b5914a7adf2c4d956804b3
naokicks39gm-source <253176292+naokicks39gm-source@users.noreply.github.com>
2026-03-07 06:37:29 +0900

    auto sync update

---
+++ packages/problem-engine/analyzePatterns.ts
@@ -0,0 +1,6 @@
+import "./printPatternStats.ts";
+import "./validatePatternIndex.ts";
+import "./validatePatternKeys.ts";
+import "./patternGapReport.ts";
+
+console.log("PATTERN ANALYSIS COMPLETE");
